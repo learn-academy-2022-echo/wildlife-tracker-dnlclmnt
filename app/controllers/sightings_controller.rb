f3e0342f7a5f51f1783b867a58eda3c121297946
dnlclmnt <dnlclmnt@gmail.com>
2022-09-19 11:19:54 -0400

    story 3 completed

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,7 +1,7 @@
 class SightingsController < ApplicationController
     #Index
     def index
-        sightings = Sighting.all
+        sightings = Sighting.where(date: params[:start_date]..params[:end_date])
         render json: sightings
     end
     #Show
@@ -40,6 +40,6 @@ class SightingsController < ApplicationController
 
     private
         def sighting_params
-            params.require(:sighting).permit(:animal_id, :latitude, :longitude, :date)
+            params.require(:sighting).permit(:animal_id, :latitude, :longitude, :date, :start_date, :end_date)
         end
 end
